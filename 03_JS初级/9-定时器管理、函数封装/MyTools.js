9e1a82bcd4a4a0cd4ea4955f955a61b06ef9b969
luozhijun <1979803683@qq.com>
2018-01-09 10:41:07 +0800

    JS_primer_9_

--- 03_JS初级/9-定时器管理、函数封装/MyTools.js
+++ 03_JS初级/9-定时器管理、函数封装/MyTools.js
@@ -17,8 +17,13 @@ function getStyle(element, styleName) {
 }
 
 /// 只适用于改变宽高、position和opacity
-function doTransform(obj, attrName, targetValue, velocity, stepLength, completion) {
-    if (obj.move_timer) return;
+function doTransform(obj, attrName, targetValue, interval, completion) {
+    // 为了避免大量的重名, 须用一定的手段区分不同的定时器, 如果重名, 会导致无法同时执行两个动画(后来的会提前return);
+    var timerName = 'transform_timer' + attrName + Math.abs(targetValue);
+    if (obj[timerName]) return;
+    var currentAttrValue = parseFloat(getStyle(obj, attrName));
+    var segmentCounts = 300;
+    var stepLength = parseFloat(Math.abs(targetValue - currentAttrValue)/(interval*segmentCounts));
     if (attrName == 'right') {
         attrName = 'left';
         stepLength = -stepLength;
@@ -27,32 +32,62 @@ function doTransform(obj, attrName, targetValue, velocity, stepLength, completio
         attrName = 'top';
         stepLength = -stepLength;
     }
-    var currentAttrValue = parseFloat(getStyle(oDiv, attrName));
     if (targetValue === currentAttrValue) return;
     var isDirectionPositive = targetValue > currentAttrValue ? true : false;
     stepLength = isDirectionPositive ? stepLength : -stepLength;
     var isCompleted = false;
-    obj.move_timer = setInterval(function () {
+    obj[timerName] = setInterval(function () {
         // 实践发现, 需放在下一次定时器触发时判断是否完成, 此时obj的样式变化才算全部完成;
-        // 如果直接在改变oDiv.style的时候停止定时器并通知回调, 这样会有一种现象:
+        // 如果直接在改变obj.style的时候停止定时器并通知回调, 这样会有一种现象:
         // 回调函数先触发, UI才得到人类视觉上的更新.
         if (isCompleted) {
-            clearInterval(obj.move_timer);
-            obj.move_timer = null;
+            clearInterval(obj[timerName]);
+            obj[timerName] = null;
             if (typeof completion === 'function') {
                 completion();
             }
             return;
         }
-        var nextAttrValue = parseFloat(getStyle(oDiv, attrName)) + stepLength;
+        var nextAttrValue = parseFloat(getStyle(obj, attrName)) + stepLength;
         if ((nextAttrValue > targetValue && isDirectionPositive) || (nextAttrValue < targetValue && !isDirectionPositive)) {
             nextAttrValue = targetValue;
             isCompleted = true;
         }
         if(attrName === 'opacity') {
-            oDiv.style[attrName] = nextAttrValue;
+            obj.style[attrName] = nextAttrValue;
         } else if (attrName === 'left' || attrName === 'top' || attrName === 'bottom' || attrName === 'right' || attrName === 'width' || attrName === 'height') {
-            oDiv.style[attrName] = nextAttrValue + 'px';
+            obj.style[attrName] = nextAttrValue + 'px';
         }
-    }, velocity ? velocity : 30);
+    }, parseFloat(interval/segmentCounts));
+}
+
+/// direction: horizontal\vertical
+function doShake(obj, direction, initialLeft, initialTop) {
+    obj.style.left = initialLeft + 'px';
+    obj.style.top = initialTop + 'px';
+
+    clearInterval(obj.shake_timer);
+
+    var movingValues = [];
+    for(var i = 30; i -= 5; i > 0) {
+        movingValues.push(i);
+        movingValues.push(-i);
+    }
+    movingValues.push(0);
+
+    var index = 0;
+    obj.shake_timer = setInterval(function () {
+        if(direction === 'vertical') {
+            var currentTop = parseFloat(getStyle(obj, 'top'));
+            obj.style.top = currentTop + movingValues[index] + 'px';
+        } else {
+            var currentLeft = parseFloat(getStyle(obj, 'left'));
+            obj.style.left = currentLeft + movingValues[index] + 'px';
+        }
+        index += 1;
+        if (index >= movingValues.length) {
+            clearInterval(obj.shake_timer);
+            obj.shake_timer = null;
+        }
+    }, 50);
 }
